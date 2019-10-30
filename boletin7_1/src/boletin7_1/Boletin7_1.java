fda1bd416b7cda00503ee474343b1a9d3d0db1cd
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-30 09:51:59 +0100

    Ejercicio 1, boletin 7

---
+++ boletin7_1/src/boletin7_1/Boletin7_1.java
@@ -0,0 +1,13 @@
+package boletin7_1;
+// @author irodriguezsteuerberg
+ 
+public class Boletin7_1 {
+
+    public static void main(String[] args) {
+int a1=30,a2=20;
+        System.out.println("Primeira expresion: "+(a2< 15 && a1 > a2 || a2 > 10));
+        System.out.println("Segunda expresion: "+(a2 < 15 && ( a1 > a2 || a2 > 10 )));
+
+    }
+    
+}
